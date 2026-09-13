e49423756bdf8c83106c1a095405ba4c4795889b
davindakhrisna <arpeggio.gns@gmail.com>
2026-09-13 15:11:33 +0700

    fix DRM

--- hosts/powerhouse/default.nix
+++ hosts/powerhouse/default.nix
@@ -70,8 +70,6 @@
           };
         };
 
-        environment.sessionVariables.AQ_DRM_DEVICES =
-          "/dev/dri/by-path/pci-0000:01:00.0-card:/dev/dri/by-path/pci-0000:00:02.0-card";
         home-manager.users.kryisnn = {...}: {
           imports = with self.homeModules; [
             home-manager
